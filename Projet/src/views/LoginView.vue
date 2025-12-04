a77acc67eeb7c42965063be1c6f7cd176cadc50b
CTRetu <Celia.tropel@etu.univ-grenoble-alpes.fr>
2025-12-04 17:55:01 +0100

    Connexion / inscription

--- Projet/src/views/LoginView.vue
+++ Projet/src/views/LoginView.vue
@@ -1,5 +1,58 @@
 <template>
-  <div class="p-10">
-    <h1>Page en cours de construction...</h1>
+  <div class="register-container">
+    <div class="register-card">
+
+      <h1>Se connecter</h1>
+
+      <form @submit.prevent="handleLogin">
+
+        <label>Email</label>
+        <input type="email" v-model="email" required />
+
+        <label>Mot de passe</label>
+        <input type="password" v-model="password" required />
+
+        <div class="keep-connected">
+          <br/>
+          <input type="checkbox" v-model="keepConnected" />
+          <span> Rester connecté</span>
+          <br/><br/>
+        </div>
+
+        <button type="submit" class="btn-primary">Se connecter</button>
+      </form>
+
+      <p class="login-text">
+        <br/>
+        Pas encore de compte ? <router-link to="/register">Créer un compte</router-link>
+      </p>
+
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      email: "",
+      password: "",
+      keepConnected: false
+    };
+  },
+  methods: {
+    handleLogin() {
+      // Partie API ou authentification à brancher plus tard
+      console.log("Tentative de connexion :", {
+        email: this.email,
+        keepConnected: this.keepConnected
+      });
+
+      // Simu connexion OK
+      alert("Connexion réussie !");
+      this.$router.push("/");
+    }
+  }
+};
+</script>
--- Projet/src/views/RegisterView.vue
+++ Projet/src/views/RegisterView.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="p-10">
-    <h1>Page en cours de construction...</h1>
+  <div class="register-container">
+    <div class="register-card">
+
+      <h1>S’inscrire</h1>
+      <form @submit.prevent="handleRegister">
+
+        <label>Pseudo</label>
+        <input type="text" v-model="pseudo" required />
+
+        <label>Email</label>
+        <input type="email" v-model="email" required />
+
+        <label>Mot de passe</label>
+        <input type="password" v-model="password" required />
+
+        <label>Confirmer le mot de passe</label>
+        <input type="password" v-model="passwordConfirm" required />
+
+        <div class="keep-connected">
+          <br/> 
+          <input type="checkbox" v-model="keepConnected" />
+          <span> Rester connecté</span>
+          <br/> <br/> 
+        </div>
+
+        <button type="submit" class="btn-primary">S'inscrire</button>
+
+      </form>
+      <p class="login-text">
+        <br/> 
+        Déjà un compte ? <router-link to="/login">Se connecter ici</router-link>
+      </p>
+
+    </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: "RegisterView",
+  data() {
+    return {
+      pseudo: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      keepConnected: false
+    };
+  },
+  methods: {
+    handleRegister() {
+      if (this.password !== this.passwordConfirm) {
+        alert("Les mots de passe ne correspondent pas.");
+        return;
+      }
+
+      // Ici tu mettras plus tard un appel API
+      console.log("Compte créé :", {
+        pseudo: this.pseudo,
+        email: this.email,
+        keepConnected: this.keepConnected
+      });
+
+      alert("Compte créé avec succès !");
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+
